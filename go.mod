bfa7bc84d92da85489c2cdc6df881906eccb6589
Masayoshi Mizutani <mizutani@cookpad.com>
2019-07-17 11:19:51 +0900

    init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/m-mizutani/deepalert-github
+
+go 1.12
+
+require (
+	github.com/aws/aws-lambda-go v1.11.1
+	github.com/aws/aws-sdk-go v1.20.20
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/objx v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20190712062909-fae7ac547cb7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	// Logger can be modified by test code
+	Logger = logrus.New()
+)
+
+type vtSecrets struct {
+	GithubToken    string `json:"github_token"`
+	GithubEndpoint string `json:"github_endpoint"`
+	GithubRepo     string `json:"github_repo"`
+}
+
+func getSecretValues(secretArn string, values interface{}) error {
+	// sample: arn:aws:secretsmanager:ap-northeast-1:1234567890:secret:mytest
+	arn := strings.Split(secretArn, ":")
+	if len(arn) != 7 {
+		return errors.New(fmt.Sprintf("Invalid SecretsManager ARN format: %s", secretArn))
+	}
+	region := arn[3]
+
+	ssn := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	}))
+	mgr := secretsmanager.New(ssn)
+
+	result, err := mgr.GetSecretValue(&secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretArn),
+	})
+
+	if err != nil {
+		return errors.Wrapf(err, "Fail to retrieve secret values: %s", secretArn)
+	}
+
+	err = json.Unmarshal([]byte(*result.SecretString), values)
+	if err != nil {
+		return errors.Wrapf(err, "Fail to parse secret values as JSON: %s", secretArn)
+	}
+
+	return nil
+}
+
+func handler(ctx context.Context, snsEvent events.SNSEvent) error {
+	for _, record := range snsEvent.Records {
+		fmt.Sprintf("%v\n", record)
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
